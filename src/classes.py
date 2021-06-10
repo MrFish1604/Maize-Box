d2f4a3def477b66bc585b75306bfa270f3723cf5
MrFish1604 <mmrfishc@gmail.com>
2021-06-10 20:11:25 +0200

    Improve Plan

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import cos, sin
+from numpy import cos, sin, array
 from math import pow, sqrt
 
 class World:
@@ -16,16 +16,25 @@ class World:
 	t0 = 0
 
 	@classmethod
-	def init(cls, plan=None):
+	def init(cls):
 		"""
 		Initialise le monde
 		"""
 		if World.notinitialized:
-			if type(plan) == Plan:
-				World.plans = [plan]
-			else:
-				World.plans = []
+			World.plans = []
 			World.notinitialized = False
+	
+	def addPlan(plan):
+		"""
+		Ajoute un plan
+		"""
+		World.plans.append(plan)
+	def addPlans(plans):
+		"""
+		Ajoute des plans
+		"""
+		for plan in plans:
+			World.plans.append(plan)
 
 	@classmethod
 	def setTime(cls, h=None, nh=None, tf=None):
@@ -52,7 +61,7 @@ class World:
 	@classmethod	
 	def create_Maizes(cls, nbr):
 		World.nbr_Maizes = nbr
-		World.maizes = [Maize(np.array([0,0,0]), np.array([0,0,0])) for i in range(nbr)]
+		World.maizes = [Maize(array([0,0,0]), array([0,0,0])) for i in range(nbr)]
 	@classmethod
 	def create_plan(cls, equaCart):
 		World.plans.append(Plan(equaCart))
@@ -62,7 +71,7 @@ class World:
 		"""
 		Evalue la distance d'une bille au plan (pour l'instant il n'y en a qu'un et c'est un vrai plan)
 		"""
-		N = (np.dot(cls.plans[iplan].equa_cart, np.array([posi[0], posi[1], posi[2], 1])))
+		N = (np.dot(cls.plans[iplan].equa, array([posi[0], posi[1], posi[2], 1])))
 		D = sqrt(np.dot(cls.plans[iplan].normal, cls.plans[iplan].normal))
 		return N/D
 	
@@ -82,9 +91,9 @@ class World:
 					# print(dplan)
 					delta = maize.R - dplan			# calcul delta
 					Kstar = 4*Maize.Estar*sqrt(maize.R*delta)/3
-					accel = (Kstar*delta/maize.masse)*np.array([cos(World.plans[0].theta)*sin(World.plans[0].psi), cos(World.plans[0].theta)*cos(World.plans[0].psi), sin(World.plans[0].theta)]) - np.array([0, World.gravity, 0])
+					accel = (Kstar*delta/maize.masse)*array([cos(World.plans[0].theta)*sin(World.plans[0].psi), cos(World.plans[0].theta)*cos(World.plans[0].psi), sin(World.plans[0].theta)]) - array([0, World.gravity, 0])
 				else:
-					accel = np.array([0, -World.gravity, 0])
+					accel = array([0, -World.gravity, 0])
 				velocity = maize.velocities[i] + World.step*accel
 				position = maize.positions[i] + World.step*velocity
 				maize.setAccel(accel, i)
@@ -101,22 +110,30 @@ class Plan:
 	Représente un plan
 	!!! Il faudra changer tout ca !!!
 	"""
-	def __init__(self, equaCart):
-		self.equa_cart = equaCart
-		self.normal = np.array([equaCart[0], equaCart[1], equaCart[2]])
-		self.psi = 0		# Faudrat les calculer, ou inverser le truc
-		self.theta = 0		# Faudrat les calculer, ou inverser le truc
-	def __str__(self):
-		return str(self.equa_cart[0]) + "x + " + str(self.equa_cart[1]) + "y + " + str(self.equa_cart[2]) + "z + " + str(self.equa_cart[3]) + " = 0"
-	def __repr__(self):
-		return "Plan([" + str(self.equa_cart[0]) + ", " + str(self.equa_cart[1]) + ", " + str(self.equa_cart[2]) + ", " + str(self.equa_cart[3]) + "])"
+	n_id = 1
+	def __init__(self, psi, theta, point):
+		self.ID = Plan.n_id
+		Plan.n_id+=1
+		self.psi = psi
+		self.theta = theta
+		self.point = point
+		self.calc_equa()
 	
-	def getNormal(self):
-		"""
-		Retourne un vecteur normal au plan
-		"""
-		return self.normal
-
+	def set_rota(self, psi, theta):
+		self.theta = theta
+		self.point = point
+		self.calc_equa()
+	
+	def calc_equa(self):
+		self.normal = array([-cos(self.theta)*sin(self.psi), cos(self.theta)*cos(self.psi), sin(self.theta)])
+		self.d = np.dot(-self.normal, self.point)
+		self.equa = array([self.normal[0], self.normal[1], self.normal[2], self.d])
+	def show2D(self, size, style="-k"):
+		x1 = self.point[0] - size*cos(self.psi)
+		y1 = self.point[1] - size*sin(self.psi)
+		x2 = self.point[0] + size*cos(self.psi)
+		y2 = self.point[1] + size*sin(self.psi)
+		plt.plot([x1, x2], [y1, y2], style)
 class Maize:
 	"""
 	This class represent a ball of maize
@@ -137,12 +154,19 @@ class Maize:
 		self.positions = np.zeros((World.nbr_steps, 3))
 		self.velocities = np.zeros((World.nbr_steps, 3))
 		self.accels = np.zeros((World.nbr_steps, 3))
+		self.sum_F = np.zeros((World.nbr_steps, 3))
 		self.positions[0] = pos
 		self.velocities[0] = vits
 		self.R = 0.005
 		self.vol = (4/3)*np.pi*pow(R, 3)
 		self.masse = self.vol*self.ro
 		Maize.Estar = Maize.young/(2-2*pow(Maize.poisson, 2))
+	
+	def PFD(self):
+		"""
+		Cherche les forces qui agissent sur la bille
+		"""
+		# Cherche les plans
 	
 	def setInit(self, pos, vits):
 		self.positions[0] = pos
@@ -165,75 +189,40 @@ class Maize:
 		"""
 		self.accels[i] = accel
 
-World.init(Plan(np.array([0,1,0,0])))
-World.setTime(h=0.0001, tf=5)
-World.create_Maizes(2)
+World.init()
+plan = Plan(0, 0, array([0,0,0]))
+World.addPlan(plan)
+World.setTime(h=0.0001, tf=3)
 
-maize = World.maizes[0]
-maize.setInit(np.array([0, 0.2, 0]), np.array([0.1,0,0]))
-World.maizes[1].setInit(np.array([0, 0.3, 0]), np.array([0.2, 0, 0]))
+World.create_Maizes(1)
+World.maizes[0].setInit(array([-0.3, 0.2, 0]), array([0.2,0,0]))
 World.process()
-
-# print(World.step)
-# print(World.nbr_steps)
-# print(World.tfinal)
-# 
-# print()
-# print(maize.R)
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-X = maize.positions[:, 0]
-Y = maize.positions[:, 1]
-
-X2 = World.maizes[1].positions[:, 0]
-Y2 = World.maizes[1].positions[:, 1]
+X = World.maizes[0].positions[:, 0]
+Y = World.maizes[0].positions[:, 1]
 
 fig = plt.figure()
-
-plt.plot([0, 1], [0, 0], "-k")
 plt.axis("equal")
+plan.show2D(0.5)
 
-bille = plt.Circle((X[0], Y[0]), radius=maize.R)
-bille2 = plt.Circle((X2[0], Y2[0]), radius=maize.R)
+circle = plt.Circle((X[0], Y[0]), radius=World.maizes[0].R)
 
-ani_h = 50
-
+ani_h=50
 nbr_frames = int(World.tfinal*ani_h*1000)
 new_h = int(ani_h*0.001/World.step)
 
-# print()
-# print("nbr_frames", nbr_frames)
-# print("World.nbr_steps", World.nbr_steps)
-# print("new_h", new_h)
-
-from time import time
-World.t0 = 0
-# ts = [0]*nbr_frames
-
-def init_a():
-	plt.gca().add_patch(bille)
-	plt.gca().add_patch(bille2)
+def init_ani():
+	fig.gca().add_patch(circle)
 
 def animate(i):
 	j = new_h*i
 	if j<np.size(X,0):
-		bille.center = (X[j], Y[j])
-		bille2.center = (X2[j], Y2[j])
-	# t = time()
-	# ts[i] = t - World.t0
-	# World.t0 = t
-	return bille
+		circle.center = (X[j], Y[j])
 
-hms = World.step*1000
-# if World.step<0.001:
-ani = FuncAnimation(fig, animate, init_func=init_a, interval=ani_h, frames=nbr_frames)
-# else:
-# ani = FuncAnimation(fig, animate, init_func=init_a, interval=World.step*1000, frames=World.nbr_steps)
+ani = FuncAnimation(fig, animate, init_func=init_ani, interval=ani_h, frames=nbr_frames)
 
 input("Press enter to display...")
 plt.show()
-
-# print("\n")
-# print(maize.accels)
