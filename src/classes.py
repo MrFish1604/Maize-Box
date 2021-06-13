783f42281fe248fbb3c68994783de266958af265
MrFish1604 <mmrfishc@gmail.com>
2021-06-13 15:07:08 +0200

    Create a Box class Show a box and animate it

--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,11 @@
 import numpy as np
 from numpy import cos, sin, array, pi, log
+from numpy.linalg import norm
 from math import pow, sqrt
+
+PIs2 = pi/2
+PIs4 = pi/4
+PIs6 = pi/6
 
 def distPlan(posi, plan):
 		"""
@@ -107,30 +112,104 @@ class Plan:
 		self.psi = psi
 		self.theta = theta
 		self.point = point
+		self.calc_normal()
 		self.calc_equa()
+		self.carX = [0,0]
+		self.carY = [0,0]
 	
 	def set_rota(self, psi, theta):
 		"""
 		Modifie les angles et calcul les équations
 		"""
+		self.psi = psi
 		self.theta = theta
-		self.point = point
+		self.calc_normal()
 		self.calc_equa()
 	
 	def calc_equa(self):
 		"""
 		Calcul les equations du plan
 		"""
-		self.normal = array([-cos(self.theta)*sin(self.psi), cos(self.theta)*cos(self.psi), sin(self.theta)])
 		self.d = np.dot(-self.normal, self.point)
 		self.equa = array([self.normal[0], self.normal[1], self.normal[2], self.d])
-		
+	def calc_normal(self):
+		self.normal = array([-cos(self.theta)*sin(self.psi), cos(self.theta)*cos(self.psi), sin(self.theta)])
+		self.normal = self.normal/norm(self.normal)
+	
+	def calc_car(self, size):
+		"""
+		Calcule des points appartenants au plan permettant de l'afficher
+		"""
+		self.carX[0] = self.point[0] - size*cos(self.psi)
+		self.carY[0] = self.point[1] - size*sin(self.psi)
+		self.carX[1] = self.point[0] + size*cos(self.psi)
+		self.carY[1] = self.point[1] + size*sin(self.psi)
+
 	def show2D(self, size, style="-k"):
-		x1 = self.point[0] - size*cos(self.psi)
-		y1 = self.point[1] - size*sin(self.psi)
-		x2 = self.point[0] + size*cos(self.psi)
-		y2 = self.point[1] + size*sin(self.psi)
-		plt.plot([x1, x2], [y1, y2], style)
+		self.calc_car(size)
+		self.repr, = plt.plot(self.carX, self.carY, style)
+		return self.repr
+	
+	def update2D(self, i, size):
+		pass
+
+class Box:
+	"""
+	Représente une boite
+	"""
+	def __init__(self, dim, ampli=PIs4, pulse=2.5133):	#	pi/4=0.7854		2pi/2.5 = 2.5133
+		self.dim = dim
+		self.demiH = self.dim[1]/2
+		self.demiL = self.dim[0]/2
+		self.ampli = ampli
+		self.pulse = pulse
+		self.bot = Plan(0, 0, np.array([0,0,0]))
+		self.top = Plan(pi, 0, np.array([0, dim[1], 0]))
+		self.left = Plan(-PIs2, 0, np.array([-self.demiL, self.demiH, 0]))
+		self.right = Plan(PIs2, 0, np.array([self.demiL, self.demiH, 0]))
+	
+	def move2D(self, i):
+		# Move bottom
+		self.bot.psi = self.ampli*sin(self.pulse*World.step*i)
+		self.bot.calc_normal()
+		self.bot.calc_equa()
+		# Move left
+		self.left.psi = (self.ampli)*sin(self.pulse*World.step*i) - PIs2
+		self.left.calc_normal()
+		self.left.point = -self.demiL*self.left.normal+ self.demiH*self.bot.normal
+		self.left.calc_equa()
+		# Move right
+		self.right.psi = (self.ampli)*sin(self.pulse*World.step*i) + PIs2
+		self.right.calc_normal()
+		self.right.point = self.demiL*self.left.normal + self.demiH*self.bot.normal
+		self.right.calc_equa()
+		# Move top
+		self.top.psi = (self.ampli)*sin(self.pulse*World.step*i)
+		self.top.calc_normal()
+		self.top.point = self.dim[1]*self.bot.normal
+		self.top.calc_equa()
+	
+	def calc_car(self):
+		self.bot.calc_car(self.demiL)
+		self.left.calc_car(self.demiH)
+		self.right.calc_car(self.demiH)
+		self.top.calc_car(self.demiL)
+	
+	def show2D(self):
+		self.r_bot = self.bot.show2D(self.demiL)
+		self.r_top = self.top.show2D(self.demiL)
+		self.r_left = self.left.show2D(self.demiH)
+		self.r_right = self.right.show2D(self.demiH)
+	
+	def update2D(self, i):
+		self.move2D(i)
+		self.calc_car()
+		self.r_bot.set_data(self.bot.carX, self.bot.carY)
+		self.r_left.set_data(self.left.carX, self.left.carY)
+		self.r_right.set_data(self.right.carX, self.right.carY)
+		self.r_top.set_data(self.top.carX, self.top.carY)
+
+
 class Maize:
 	"""
 	This class represent a ball of maize
@@ -218,7 +297,7 @@ class Maize:
 World.init()
 plan = Plan(0, 0, array([0,0,0]))
 World.addPlan(plan)
-World.setTime(h=0.00001, tf=3)
+World.setTime(h=0.001, tf=3)
 
 World.create_Maizes(1)
 World.maizes[0].setInit(array([-0.3, 0.2, 0]), array([0.2,0,0]))
@@ -232,7 +311,10 @@ Y = World.maizes[0].positions[:, 1]
 
 fig = plt.figure()
 plt.axis("equal")
-plan.show2D(0.5)
+# plan.show2D(0.5)
+
+box = Box((1, 0.3))
+box.show2D()
 
 circle = plt.Circle((X[0], Y[0]), radius=World.maizes[0].R)
 
@@ -244,9 +326,10 @@ def init_ani():
 	fig.gca().add_patch(circle)
 
 def animate(i):
-	j = new_h*i
+	j = (new_h*i)%np.size(X,0)
 	if j<np.size(X,0):
 		circle.center = (X[j], Y[j])
+		box.update2D(j)
 
 ani = FuncAnimation(fig, animate, init_func=init_ani, interval=ani_h, frames=nbr_frames)
 
