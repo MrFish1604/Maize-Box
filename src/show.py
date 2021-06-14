1ee88f0e31e3aa2c0e456a92207810bf7240a29a
MrFish1604 <mmrfishc@gmail.com>
2021-06-14 23:49:59 +0200

    Simulation with 10 maizes in the moving box Works well, slow to process

--- src/show.py
+++ src/show.py
@@ -1,10 +1,14 @@
 from classes import World, Box
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, FFMpegWriter, PillowWriter
 from numpy import size
 from time import time
 
-SAVE_PATH = "../simulations/2m_BnM_1e-6"
+# plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+
+SIMU_NAME = "10m_BnM_1e-5"
+SAVE_PATH = "../simulations/" + SIMU_NAME
+VIDEO_PATH = "../video/" + SIMU_NAME + ".mp4"
 
 t0 = time()
 print(World.load_save(SAVE_PATH))
@@ -24,23 +28,32 @@ ani_h=50
 nbr_frames = int(2*World.tfinal*ani_h*1000)
 new_h = int(ani_h*0.001/(2*World.step))
 
-maize = World.maizes[0]
-maize2 = World.maizes[1]
+# maize = World.maizes[0]
+# maize2 = World.maizes[1]
 
-circle = plt.Circle((maize.positions[0,0], maize.positions[0,1]), radius=maize.R, color=(1,0,0))
-circle2 = plt.Circle((maize2.positions[0,0], maize2.positions[0,1]), radius=maize2.R)
+maizes = World.maizes
+
+circles = [plt.Circle((maizes[i].positions[0,0], maizes[i].positions[0,1]), radius=maizes[i].R) for i in range(World.nbr_Maizes)]
+# circle = plt.Circle((maize.positions[0,0], maize.positions[0,1]), radius=maize.R, color=(1,0,0))
+# circle2 = plt.Circle((maize2.positions[0,0], maize2.positions[0,1]), radius=maize2.R)
 
 def init_ani():
-	fig.gca().add_patch(circle)
-	fig.gca().add_patch(circle2)
+	for i in range(World.nbr_Maizes):
+		fig.gca().add_patch(circles[i])
 
 def animate(i):
 	j = (new_h*i)%World.nbr_steps
-	circle.center = (maize.positions[j, 0], maize.positions[j, 1])
-	circle2.center = (maize2.positions[j, 0], maize2.positions[j, 1])
-	# box.update2D(j)
+	for k in range(World.nbr_Maizes):
+		circles[k].center = (maizes[k].positions[j, 0], maizes[k].positions[j, 1])
+	box.update2D(j)
 
-ani = FuncAnimation(fig, animate, init_func=init_ani, interval=ani_h, frames=nbr_frames)
+ani = FuncAnimation(fig, animate, init_func=init_ani, interval=ani_h, frames=nbr_frames, repeat=False)
+
+# Save the video
+# writervideo = FFMpegWriter(fps=60)
+# writervideo = PillowWriter(fps=30) 
+# ani.save(VIDEO_PATH, writer=writervideo)
+
 plt.gca().set_xlim(-0.7, 0.7)
 plt.gca().set_ylim(-0.6, 0.8)
 input("Press enter to display...")
